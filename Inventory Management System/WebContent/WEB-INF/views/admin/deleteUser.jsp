c7d21dc7ccd51c0b31260301e9e1365dcdd4fcf6
harsha.m <harsha.m@PENNANT-VSP179.pennant.local>
2023-06-27 15:18:58 +0530

    Hari's Modified Javascripts and styles

--- Inventory Management System/WebContent/WEB-INF/views/admin/deleteUser.jsp
+++ Inventory Management System/WebContent/WEB-INF/views/admin/deleteUser.jsp
@@ -1,110 +1,7 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<style>
-   .center {
-   display: flex;
-   justify-content: center;
-   align-items: center;
-   height: 100vh;
-   }
-</style>
-<script>
-$(document).ready(function() {
-    $.ajax({
-        url: "getUser",
-        method: "POST",
-        dataType: "json",
-        success: function(data) {
-            console.log(data);
-            var dropdown = $("#userType");
-            dropdown.empty(); // Clear existing options
-
-            var defaultOption = $("<option>").val("").text("Select User Type");
-            dropdown.append(defaultOption);
-
-            var inventoryFound = false;
-            var procurementFound = false;
-
-            data.forEach(function(user) {
-                if (user.userType === "inventory" && !inventoryFound) {
-                    var option = $("<option>").val(user.userType).text(user.userType);
-                    dropdown.append(option);
-                    inventoryFound = true;
-                } else if (user.userType === "procurement" && !procurementFound) {
-                    var option = $("<option>").val(user.userType).text(user.userType);
-                    dropdown.append(option);
-                    procurementFound = true;
-                }
-            });
-        },
-        error: function() {
-            console.log("Error");
-        }
-    });
-});
-
-$(document).ready(function() {
-    $("#userType").change(function() {
-        var userType = $(this).val();
-        if (userType) {
-            $.ajax({
-                url: "getUserData",
-                type: "POST",
-                dataType: "json",
-                contentType: "application/json",
-                data: JSON.stringify({
-                    "userType": userType
-                }),
-                success: function(response) {
-                    console.log(response);
-                    var userIdDropdown = $("#userId");
-                    userIdDropdown.empty(); // Clear existing options
-
-                    var defaultOption = $("<option>").val("").text("Select User Name");
-                    userIdDropdown.append(defaultOption);
-
-                    response.forEach(function(user) {
-                        var option = $("<option>").val(user.userId).text(user.userId + " - " + user.userName);
-                        userIdDropdown.append(option);
-                    });
-                },
-                error: function() {
-                    alert("An error occurred while fetching user information.");
-                }
-            });
-        }
-    });
-});
-
-function deleteUser() {
-    // Retrieve form data
-    var userId = $("#userId").val();
-
-    // Confirmation dialog
-    var confirmationMessage = "User ID: " + userId + "\n\nAre you sure you want to delete this user?";
-    var userConfirmed = window.confirm(confirmationMessage);
-
-    if (userConfirmed) {
-        // Send the data to the backend using AJAX
-        $.ajax({
-            url: "deleteUserData",
-            type: "POST",
-            contentType: "application/json",
-            data: JSON.stringify({
-                "userId": userId,
-            }),
-            success: function(response) {
-                alert("User deleted successfully.");
-                // Perform any additional actions after deletion
-            },
-            error: function() {
-                alert("An error occurred while deleting the user.");
-            }
-        });
-    }
-
-    return false; // Prevent form submission
-}
-</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="./adminHome/scripts/deleteUserScripts.js"></script>
 <div class="center">
    <div class="col-md-6">
       <h2 class="mb-4">Delete User</h2>
@@ -125,5 +22,4 @@ function deleteUser() {
       </form>
    </div>
 </div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
--- Inventory Management System/src/main/dal/users/WarehouseUsersDAL.java
+++ Inventory Management System/src/main/dal/users/WarehouseUsersDAL.java
@@ -18,7 +18,7 @@ public class WarehouseUsersDAL implements WarehouseUsersDAO {
 
 	@PersistenceContext
 	private EntityManager entityManager;
-	
+
 	// Saving Warehouse User Information
 	@Transactional
 	public void saveUser(User user) {
@@ -28,48 +28,73 @@ public class WarehouseUsersDAL implements WarehouseUsersDAO {
 			throw new IllegalArgumentException("User with the same unique key already exists.");
 		}
 	}
-	
+
 	// Soft Deleting the Warehouse Users
 	@Transactional
 	public User deleteUser(UserId user) {
-		User existingUser = entityManager.find(User.class, user.getUserId()); // Find the User object with the given userId
-		existingUser.setStatus("Inactive"); // Set the status of the existing User object to "Inactive"
-		entityManager.merge(existingUser); // Merge the changes into the database
+		// Find the User object with the given userId
+		User existingUser = entityManager.find(User.class, user.getUserId());
+		// Set the status of the existing User object to "Inactive"
+		existingUser.setStatus("Inactive");
+		// Merge the changes into the database
+		entityManager.merge(existingUser);
 		return existingUser; // Return the updated User object
 	}
-	
+
 	// Getting all Active Warehouse Users
 	@Transactional
 	public List<User> getAllActiveUsers() {
-		List<User> users = entityManager.createQuery("SELECT u FROM User u WHERE status = 'active'").getResultList(); // Retrieve all User objects with status "active"
+		List<User> users = entityManager.createQuery("SELECT u FROM User u WHERE status = 'active'").getResultList(); // Retrieve
+																														// all
+																														// User
+																														// objects
+																														// with
+																														// status
+																														// "active"
 		return users; // Return the list of active User objects
 	}
+
 	// Getting all Warehouse Users
-		@Transactional
-		public List<User> getAllUsers() {
-			List<User> listUsers = entityManager.createQuery("SELECT u FROM User u").getResultList(); // Retrieve all User objects from the database
-			return listUsers; // Return the list of User objects
-		}
-	//Getting User data based on the selected user type and status
+	@Transactional
+	public List<User> getAllUsers() {
+		List<User> listUsers = entityManager.createQuery("SELECT u FROM User u").getResultList(); // Retrieve all User
+																									// objects from the
+																									// database
+		return listUsers; // Return the list of User objects
+	}
+
+	// Getting User data based on the selected user type and status
 	@Override
 	public List<UserTypeStatus> getUserByTypeStatus(UserTypeStatus userTypeStatus) {
 		// TODO Auto-generated method stub
-		List<UserTypeStatus> userTypeStatusList = entityManager.createQuery("SELECT u FROM User u WHERE u.status =:userStatus and u.userType=:userType ").setParameter("userStatus",userTypeStatus.getUserStatus()).setParameter("userType",userTypeStatus.getUserType()).getResultList(); // Retrieve all User objects with status "active"
-        return userTypeStatusList;
+		List<UserTypeStatus> userTypeStatusList = entityManager
+				.createQuery("SELECT u FROM User u WHERE u.status =:userStatus and u.userType=:userType ")
+				.setParameter("userStatus", userTypeStatus.getUserStatus())
+				.setParameter("userType", userTypeStatus.getUserType()).getResultList(); // Retrieve all User objects
+																							// with status "active"
+		return userTypeStatusList;
 	}
-   //getting user data based on UserStatus
+
+	// getting user data based on UserStatus
 	@Override
 	public List<UserTypeStatus> getFilteredDataByStatus(UserTypeStatus userTypeStatus) {
 		// TODO Auto-generated method stub
-		List<UserTypeStatus> userTypeStatusList = entityManager.createQuery("SELECT u FROM User u WHERE u.status =:userStatus").setParameter("userStatus",userTypeStatus.getUserStatus()).getResultList(); // Retrieve all User objects with status "active"
+		List<UserTypeStatus> userTypeStatusList = entityManager
+				.createQuery("SELECT u FROM User u WHERE u.status =:userStatus")
+				.setParameter("userStatus", userTypeStatus.getUserStatus()).getResultList(); // Retrieve all User
+																								// objects with status
+																								// "active"
 		return userTypeStatusList;
 	}
-   //getting user Data based on userType
+
+	// getting user Data based on userType
 	@Override
 	public List<UserTypeStatus> getFilteredDataByType(UserTypeStatus userTypeStatus) {
-		List<UserTypeStatus> userTypeStatusList = entityManager.createQuery("SELECT u FROM User u WHERE u.userType=:userType").setParameter("userType",userTypeStatus.getUserType()).getResultList(); // Retrieve all User objects with status "active"
+		List<UserTypeStatus> userTypeStatusList = entityManager
+				.createQuery("SELECT u FROM User u WHERE u.userType=:userType")
+				.setParameter("userType", userTypeStatus.getUserType()).getResultList(); // Retrieve all User objects
+																							// with status "active"
 		return userTypeStatusList;
 	}
-	
 
 }
