aba09b2462fcf6caed65e84d727911ee720ece66
sanjana.v <sanjana.v@pennant-vsp128.pennant.local>
2023-06-29 12:30:00 +0530

    updated without errors

--- Inventory Management System/src/main/dao/adjustments/AdjustmentsDAO.java
+++ Inventory Management System/src/main/dao/adjustments/AdjustmentsDAO.java
@@ -2,7 +2,6 @@ package main.dao.adjustments;
 
 import java.util.List;
 
-import main.dal.adjustments.AdjustmentsException;
 import main.models.adjustmentsModels.entities.AdjustmentsList;
 import main.models.adjustmentsModels.inputModels.AdjustmentsFilterInput;
 import main.models.adjustmentsModels.inputModels.AdjustmentsInputList;
@@ -12,40 +11,35 @@ import main.models.adjustmentsModels.outputModels.AdjustmentsFilterOutput;
 public interface AdjustmentsDAO {
 
 	// The getAdjustments method is used to retrieve a list of all the Adjustments
-	public List<AdjustmentsList> getAdjustments() throws AdjustmentsException;
+	public List<AdjustmentsList> getAdjustments();
 
 	/*
 	 * The saveAdjustments method is responsible for persisting adjustments details (product category, product name,
 	 * batch no, original stock, updated stock and reason) to the database
 	 */
-	public boolean saveAdjustments(AdjustmentsList adjustmentsList) throws AdjustmentsException;
+	public boolean saveAdjustments(AdjustmentsList adjustmentsList);
 
 	// The getAdjustmentProductsList method is for displaying the list of all the products in each Adjustment ID
-	public List<AdjustmentProductsListData> getAdjustmentProductsList(AdjustmentsInputList adjustmentid)
-			throws AdjustmentsException;
+	public List<AdjustmentProductsListData> getAdjustmentProductsList(AdjustmentsInputList adjustmentid);
 
 	// This method filters Adjustment ID's by product category Id, product Id and From date
 	public List<AdjustmentsFilterOutput> getFilterDataByCategoryIdProductIdFrom(
-			AdjustmentsFilterInput adjustmentsFilterInput) throws AdjustmentsException;
+			AdjustmentsFilterInput adjustmentsFilterInput);
 
 	// This method filters Adjustment ID's by product category Id and product Id
 	public List<AdjustmentsFilterOutput> getFilterDataByCategoryIdProductId(
-			AdjustmentsFilterInput adjustmentsFilterInput) throws AdjustmentsException;
+			AdjustmentsFilterInput adjustmentsFilterInput);
 
 	// This method filters Adjustment ID's by product category Id and From Date
-	public List<AdjustmentsFilterOutput> getFilterDataByCategoryIdFrom(AdjustmentsFilterInput adjustmentsFilterInput)
-			throws AdjustmentsException;
+	public List<AdjustmentsFilterOutput> getFilterDataByCategoryIdFrom(AdjustmentsFilterInput adjustmentsFilterInput);
 
 	// This method filters Adjustment ID's by product category Id
-	public List<AdjustmentsFilterOutput> getFilterDataByCategoryId(AdjustmentsFilterInput adjustmentsFilterInput)
-			throws AdjustmentsException;
+	public List<AdjustmentsFilterOutput> getFilterDataByCategoryId(AdjustmentsFilterInput adjustmentsFilterInput);
 
 	// This method filters Adjustment ID's by From date
-	public List<AdjustmentsFilterOutput> getFilterDataByFrom(AdjustmentsFilterInput adjustmentsFilterInput)
-			throws AdjustmentsException;
+	public List<AdjustmentsFilterOutput> getFilterDataByFrom(AdjustmentsFilterInput adjustmentsFilterInput);
 
 	// This method filters Adjustment ID's by To date
-	public List<AdjustmentsFilterOutput> getFilterDataByTo(AdjustmentsFilterInput adjustmentsFilterInput)
-			throws AdjustmentsException;
+	public List<AdjustmentsFilterOutput> getFilterDataByTo(AdjustmentsFilterInput adjustmentsFilterInput);
 
 }
